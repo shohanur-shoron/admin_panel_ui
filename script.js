d9a7555a16079efcccefc4bfb96a3cf4dc3baa8f
Shohanur Rahman Shoron <158572434+shohanur-shoron@users.noreply.github.com>
2025-10-02 15:23:51 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleBtn = document.querySelector('.toggle-btn');
-    const sidebar = document.querySelector('.sidebar');
-
-    toggleBtn.addEventListener('click', function() {
-        sidebar.classList.toggle('collapsed');
-    });
-});
